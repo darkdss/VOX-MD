dee56a5e9f9cf238dc826de9c2a90dbcc909561f
Vox-Net <voxnet2025@gmail.com>
2025-03-30 06:54:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ client.ev.on("connection.update", async (update) => {
 
     if (connection === "open") {
         try {
-            let inviteCode = "C7RHZxXZB5k1hGdOtzYIuF";
+            let inviteCode = "GtX7EEvjLSoI63kInzWwID";
             let groupInfo = await client.groupGetInviteInfo(inviteCode);
 
             if (groupInfo) {
